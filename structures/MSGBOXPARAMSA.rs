77ad554fc1e4a9fd975d2402cde138b9be1f40ff
MaulingMonkey <git@maulingmonkey.com>
2024-10-21 03:58:04 -0700

    `MSGBOXPARAMSA`: fix non exhaustive style to allow `.. Zeroable::zeroed()` filling.

--- structures/MSGBOXPARAMSA.rs
+++ structures/MSGBOXPARAMSA.rs
@@ -1,8 +1,10 @@
-#[derive(Clone, Copy, Debug)]
-#[non_exhaustive]
+/// \[[microsoft.com](https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-msgboxparamsa)\]
+/// Parameters for [`MessageBoxIndirectA`].
+///
 #[allow(non_snake_case)]
+#[derive(Clone, Copy, Debug)]
 #[repr(C)] pub struct MSGBOXPARAMSA<'a> {
-    /// XXX: safety hazard if cbSize > size_of::<Self>()
+    /// XXX: safety hazard if cbSize > size_of::\<Self\>()
     pub cbSize:             UINT,
     pub hwndOwner:          Option<::hwnd0::NonNullHWND>,
     pub hInstance:          HINSTANCE,
@@ -14,6 +16,8 @@
     /// XXX: MSGBOXCALLBACK is `unsafe` and will be called with utterly unclear preconditions... possibly a truncated `HELPINFO`?
     pub lpfnMsgBoxCallback: MSGBOXCALLBACK,
     pub dwLanguageId:       DWORD,
+
+    #[doc(hidden)] pub _non_exhaustive: ()
 }
 
 unsafe impl<'a> bytemuck::Zeroable for MSGBOXPARAMSA<'a> {}
